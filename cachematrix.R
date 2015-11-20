8473f77d271dd7f355cc6064332689fd77f43e49
alegoubin <alegoubin.public@gmail.com>
2015-11-20 22:04:08 +0100

    Arnaud Legoubin 20151119
    Version 1.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Function to handle cached matrix
+makeCacheMatrix <- function(mat = matrix()) {
+  ## mat has to be a square matrix to work
+  
+  ## initialize inverted matrix to null in current scope
+  matinv <- NULL
+  
+  ## set value to calling environment
+  set <- function(y) {
+    mat <<- y
+    matinv <<- NULL
+  }
+  
+  ## retrieve object
+  get <- function() mat
+  
+  ## set inverted matrix once calculated
+  setinv <- function(solve) matinv <<- solve
+  
+  ## attempt to retrieve stored inverted matrix
+  getinv <- function() {matinv}
+  
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##function that either retrieves from cache or computes inverted matrix
+cacheSolve <- function(mat, ...) {
+  ## mat is the matrix used by makeCacheMatrix function
+  
+  ## retrieve inverted matrix from cache if exists
+  invertedM <- mat$getinv()
+  
+  if(!is.null(invertedM)) {
+    message("getting cached inverted matrix")
+    return(invertedM)
+  }
+  else
+  {
+    
+    ## otherwise extract stored matrix
+    matget <- mat$get()
+    
+    ## and compute inverted matrix
+    invertedM <- solve(matget)
+    mat$setinv(invertedM)
+    
+    message("stored inverted matrix in cache")
+    
+  }  
+  
+  
+  
 }
